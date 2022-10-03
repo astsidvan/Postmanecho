a9bccc8a33a0a2c0d2b78194a187624c43fb917a
astsidvan <astsidvan@gmail.com>
2022-10-03 15:17:30 +0300

    init

---
+++ src/test/java/PostTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+public class PostTest {
+    @Test
+    void PostTest(){
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body(/* --> ваша проверка здесь <-- */)
+        ;
+    }
+}
